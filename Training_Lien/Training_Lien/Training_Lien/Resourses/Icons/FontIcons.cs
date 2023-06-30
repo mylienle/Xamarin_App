0603f2479d7c55105bc31600989e514824be6d89
BeNienNien <lien.lethimy0810@gmail.com>
2023-06-30 16:23:36 +0700

    changed gitignore

---
+++ Training_Lien/Training_Lien/Training_Lien/Resourses/Icons/FontIcons.cs
@@ -0,0 +1,19 @@
+﻿namespace Training_Lien
+{
+    public static class IconFont
+    {
+        public const string Search = "\ue800";
+        public const string HeartEmpty = "\ue801";
+        public const string Heart = "\ue802";
+        public const string User = "\ue803";
+        public const string OkCircled = "\ue804";
+        public const string CancelCircled = "\ue805";
+        public const string StarEmpty = "\ue806";
+        public const string Key = "\ue807";
+        public const string Lock = "\ue808";
+        public const string LockOpen = "\ue809";
+        public const string MailAlt = "\uf0e0";
+        public const string LockOpenAlt = "\uf13e";
+        public const string UserCircleO = "\uf2be";
+    }
+}
